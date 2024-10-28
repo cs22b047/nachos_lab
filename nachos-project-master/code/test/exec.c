84c9dacce0f6a4aba248e1257004eb9876f1a705
cs22b047 <cs22b047@iittp.ac.in>
2024-10-29 03:17:20 +0530

    -

--- nachos-project-master/code/test/exec.c
+++ nachos-project-master/code/test/exec.c
@@ -9,7 +9,7 @@
 int main() {
     int pid,pid2,pid3;
     int i;
-    pid = Exec("num_io",1);
+    pid = Exec("num_io",11);
     if (pid < 0) {
         Write("Exec failed: ", 14, stdout);
         PrintNum(pid);
@@ -19,13 +19,13 @@ int main() {
         Write("Exec failed: ", 14, stdout);
         PrintNum(pid2);
     }
-    pid3 = Exec("num_io3",3);
+    pid3 = Exec("num_io3",-3);
     if (pid3 < 0) {
         Write("Exec failed: ", 14, stdout);
         PrintNum(pid3);
     } 
     while(1){
-	    for(i=0;i<3000000;i++);
+	    for(i=0;i<300000;i++);
 	    PrintString("In prog2: [exec]\n");
     }
 
